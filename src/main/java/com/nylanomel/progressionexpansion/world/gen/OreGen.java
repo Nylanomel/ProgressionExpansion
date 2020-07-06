292bf2410a84999ca91a741c414f856148e6acb7
Nylanomel <36909109+Nylanomel@users.noreply.github.com>
2020-07-06 11:43:04 -0400

    Did nether orespawn

--- src/main/java/com/nylanomel/progressionexpansion/world/gen/OreGen.java
+++ src/main/java/com/nylanomel/progressionexpansion/world/gen/OreGen.java
@@ -13,15 +13,31 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class OreGen {
     public static void generateOre() {
         for(Biome biome : ForgeRegistries.BIOMES) {
-            ConfiguredPlacement customConfig = Placement.COUNT_RANGE.configure(new CountRangeConfig(10, 12, 12, 70));
+            ConfiguredPlacement copper = Placement.COUNT_RANGE.configure(new CountRangeConfig(10, 12, 12, 70));
+            ConfiguredPlacement silver = Placement.COUNT_RANGE.configure(new CountRangeConfig(9, 12, 12, 70));
+            ConfiguredPlacement cobalt = Placement.COUNT_RANGE.configure(new CountRangeConfig(8, 0, 0, 255));
+            ConfiguredPlacement mythril = Placement.COUNT_RANGE.configure(new CountRangeConfig(7, 0, 0, 255));
+            ConfiguredPlacement trueSilver = Placement.COUNT_RANGE.configure(new CountRangeConfig(6, 0, 0, 255));
             // Copper Ore
             biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
             Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-                    RegistryHandler.COPPER_ORE_BLOCK.get().getDefaultState(), 8)).withPlacement(customConfig));
+                    RegistryHandler.COPPER_ORE_BLOCK.get().getDefaultState(), 8)).withPlacement(copper));
             // Silver Ore
             biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
             Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-                    RegistryHandler.SILVER_ORE_BLOCK.get().getDefaultState(), 8)).withPlacement(customConfig));
+                    RegistryHandler.SILVER_ORE_BLOCK.get().getDefaultState(), 8)).withPlacement(silver));
+            // Cobalt Ore
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+            Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK,
+                    RegistryHandler.COBALT_ORE_BLOCK.get().getDefaultState(), 4)).withPlacement(cobalt));
+            // Mythril Ore
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+            Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK,
+                    RegistryHandler.MYTHRIL_ORE_BLOCK.get().getDefaultState(), 4)).withPlacement(mythril));
+            // True Silver Ore
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+            Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK,
+                    RegistryHandler.TRUE_SILVER_ORE_BLOCK.get().getDefaultState(), 4)).withPlacement(trueSilver));
         }
     }
 }

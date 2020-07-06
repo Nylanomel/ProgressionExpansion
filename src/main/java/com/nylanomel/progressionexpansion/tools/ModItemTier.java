ac1b8febddc682fa73ff455bdefeace6136f3121
Nylanomel <36909109+Nylanomel@users.noreply.github.com>
2020-07-06 13:47:05 -0400

    new mod item tiers

--- src/main/java/com/nylanomel/progressionexpansion/tools/ModItemTier.java
+++ src/main/java/com/nylanomel/progressionexpansion/tools/ModItemTier.java
@@ -10,6 +10,21 @@ public enum ModItemTier implements IItemTier {
 
     COPPER(2, 225, 6.5F, 0.0F, 15, () -> {
         return Ingredient.fromItems(RegistryHandler.COPPER_PICKAXE.get());
+    }),
+    SILVER(2, 200, 7.0F, 0.0F, 16, () -> {
+        return Ingredient.fromItems(RegistryHandler.SILVER_PICKAXE.get());
+    }),
+    STEEL(3, 3021, 7.2F, 0.0F, 8, () -> {
+        return Ingredient.fromItems(RegistryHandler.STEEL_PICKAXE.get());
+    }),
+    COBALT(4, 3021, 9.0F, 0.0F, 20, () -> {
+        return Ingredient.fromItems(RegistryHandler.COBALT_PICKAXE.get());
+    }),
+    MYTHRIL(5, 3906, 10.0F, 0.0F, 22, () -> {
+        return Ingredient.fromItems(RegistryHandler.MYTHRIL_PICKAXE.get());
+    }),
+    TRUE_SILVER(6, 4591, 11.0F, 0.0F, 24, () -> {
+        return Ingredient.fromItems(RegistryHandler.TRUE_SILVER_PICKAXE.get());
     });
 
     private final int harvestLevel;
